625324f3a8856a8f32cc115505bbb95311788de2
stvpn <148885238+stvpn@users.noreply.github.com>
2023-11-08 08:27:07 +0700

    Add files via upload

--- premi.sh
+++ premi.sh
@@ -715,9 +715,9 @@ account default
 host smtp.gmail.com
 port 587
 auth on
-user oceantestdigital@gmail.com
-from oceantestdigital@gmail.com
-password jokerman77 
+user -
+from -
+password -
 logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
